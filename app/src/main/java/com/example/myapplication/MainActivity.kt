0735ead5943d9b65e08913c07ed83f15cf95b21d
Second lesson <e3251105@gmail.com>
2023-06-05 22:15:56 +0300

    Second lesson

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,29 +14,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import java.util.logging.Level
 import java.util.logging.Logger
 
 class MainActivity : ComponentActivity() {
 
-   lateinit var  textView:TextView
-    lateinit var editText:TextView
-    lateinit var button:Button
 
-    var logger=Logger.getLogger("main-activity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-setContentView(R.layout.layout)
+        setContentView(R.layout.constraint_layout)
 
-        textView =findViewById<TextView>(R.id.textView2)
-        editText =findViewById<TextView>(R.id.editTextTextEmailAddress2)
-        button=findViewById(R.id.button2)
-
-        button.setOnClickListener({view-> logger.log(Level.INFO,"EditText"+editText?.text)
-            editText.setText("aaaaaaaaaaa")
-            editText.setBackgroundColor(0xFFFF87FFF.toInt())
-        })
     }
 }
 
